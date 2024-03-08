925e5e2dbf890e6d7a7d181d0fe68b3b4fbc11c9
boyonk913 <42876197+boyonk913@users.noreply.github.com>
2024-03-08 16:04:32 +0100

    Backport to 1.17

--- src/main/java/com/boyonk/musicsync/mixin/MixinMinecraftServer.java
+++ src/main/java/com/boyonk/musicsync/mixin/MixinMinecraftServer.java
@@ -2,12 +2,16 @@ package com.boyonk.musicsync.mixin;
 
 import com.boyonk.musicsync.ServerMusicTracker;
 import com.boyonk.musicsync.ServerMusicTrackerHolder;
+import com.mojang.authlib.GameProfileRepository;
+import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.datafixers.DataFixer;
 import net.minecraft.resource.ResourcePackManager;
+import net.minecraft.resource.ServerResourceManager;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.SaveLoader;
 import net.minecraft.server.WorldGenerationProgressListenerFactory;
-import net.minecraft.util.ApiServices;
+import net.minecraft.util.UserCache;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.world.SaveProperties;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -24,7 +28,7 @@ public class MixinMinecraftServer implements ServerMusicTrackerHolder {
 	ServerMusicTracker musicsync$musicTracker;
 
 	@Inject(method = "<init>", at = @At("TAIL"))
-	void musicsync$createMusicTracker(Thread serverThread, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, Proxy proxy, DataFixer dataFixer, ApiServices apiServices, WorldGenerationProgressListenerFactory worldGenerationProgressListenerFactory, CallbackInfo ci) {
+	void musicsync$createMusicTracker(Thread serverThread, DynamicRegistryManager.Impl registryManager, LevelStorage.Session session, SaveProperties saveProperties, ResourcePackManager dataPackManager, Proxy proxy, DataFixer dataFixer, ServerResourceManager serverResourceManager, MinecraftSessionService sessionService, GameProfileRepository gameProfileRepo, UserCache userCache, WorldGenerationProgressListenerFactory worldGenerationProgressListenerFactory, CallbackInfo ci) {
 		this.musicsync$musicTracker = new ServerMusicTracker((MinecraftServer) (Object) this);
 	}
 
