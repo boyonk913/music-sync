9a069092f31d31b7e97658a5f046e8477e54ced8
boyonk913 <42876197+boyonk913@users.noreply.github.com>
2024-04-30 11:41:41 +0200

    - Add /music (permission level 3) command to enable, disable and reset server music tracker - Fixed crash relating to incorrect mixin configuration

---
+++ src/main/java/com/boyonk/musicsync/server/command/MusicCommand.java
@@ -0,0 +1,55 @@
+package com.boyonk.musicsync.server.command;
+
+import com.boyonk.musicsync.ServerMusicTracker;
+import com.boyonk.musicsync.ServerMusicTrackerHolder;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import net.minecraft.server.command.ServerCommandSource;
+
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+
+public class MusicCommand {
+	public static void register(CommandDispatcher<ServerCommandSource> dispatcher, boolean dedicated) {
+		dispatcher.register(literal("music")
+				.requires(source -> source.hasPermissionLevel(3))
+				.then(literal("reset")
+						.executes(ctx -> executeReset(ctx.getSource(), ServerMusicTracker.DEFAULT_TIME_UNTIL_NEXT_SONG))
+						.then(argument("delay", IntegerArgumentType.integer(0))
+								.executes(ctx -> executeReset(ctx.getSource(), IntegerArgumentType.getInteger(ctx, "delay")))
+						)
+				)
+				.then(literal("enable")
+						.executes(ctx -> executeEnable(ctx.getSource()))
+				)
+				.then(literal("disable")
+						.executes(ctx -> executeDisable(ctx.getSource()))
+				)
+		);
+	}
+
+
+	private static int executeReset(ServerCommandSource source, int delay) {
+		ServerMusicTracker musicTracker = ((ServerMusicTrackerHolder) source.getMinecraftServer()).getMusicTracker();
+
+		musicTracker.stop(delay);
+		return 1;
+	}
+
+	private static int executeEnable(ServerCommandSource source) {
+		ServerMusicTracker musicTracker = ((ServerMusicTrackerHolder) source.getMinecraftServer()).getMusicTracker();
+
+		musicTracker.enable();
+
+		return 1;
+	}
+
+	private static int executeDisable(ServerCommandSource source) {
+		ServerMusicTracker musicTracker = ((ServerMusicTrackerHolder) source.getMinecraftServer()).getMusicTracker();
+
+		musicTracker.disable();
+		return 0;
+	}
+
+
+}
